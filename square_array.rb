25458e45388171a623e67251bc9418b42fc0139f
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-08-08 16:18:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  
+  
+  
 end
+
+def square_array(array)
+  array = [1,2,3,4,5]
+  array.each do |a|
+    new_array.push (a*a)
+  return new_array
+  end
+  end
